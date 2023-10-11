f0e8a823f1548c10306bfa2edc018e71aaee4bbb
Santiago Donoso <santiagodonoso@gmail.com>
2023-10-11 10:08:20 +0200

    ...

--- views/_header.php
+++ views/_header.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,4 +15,11 @@
   <a href="/">Home</a>
   <a href="/login">Login</a>
   <a href="/logout">Logout</a>
+
+  <?php if( $_SESSION['user']['user_role_name'] != 'admin' ): ?>
+    <a href="/users">Users</a>
+    <a href="/employees">Employes</a>
+    <a href="/partners">partners</a>
+  <?php endif ?>
+
 </nav>
