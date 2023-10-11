308b1efdf9b72b6ceac76bccab1767269987dfc8
Santiago Donoso <santiagodonoso@gmail.com>
2023-10-11 09:27:25 +0200

    ...

--- views/login.php
+++ views/login.php
@@ -8,7 +8,17 @@ function login(){
   // TODO: validate
   $user_email = $_POST['user_email'];
   $user_password = $_POST['user_password'];
-  
+  $db = _db();
+  $q = $db->prepare('SELECT * FROM users WHERE user_email = :user_email');
+  $q->bindValue(':user_email', $_POST['user_email']);
+  $q->execute();
+  $user = $q->fetch();
+  if( ! password_verify($_POST['user_password'], $user['user_password']) ){
+    echo 'Try again';
+    return;
+  }
+  session_start();
+  echo json_encode($user);
 }
 
 
@@ -19,7 +29,7 @@ require_once __DIR__.'/_header.php';
 <main>
   <form method="POST">
     <input name="user_email" type="text" placeholder="email">
-    <input name="user_password" type="password" placeholder="password">
+    <input name="user_password" type="text" placeholder="password">
     <button>Login</button>
   </form>
 </main>
