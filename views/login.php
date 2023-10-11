23f8d8ecb2837e7a69c63c585d007d20fc4f4813
Santiago Donoso <santiagodonoso@gmail.com>
2023-10-11 09:04:10 +0200

    ...

--- views/login.php
+++ views/login.php
@@ -1,9 +1,18 @@
 <?php
+
+if($_POST){
+  echo 'xxxxxxxxxxxxxxxxxx';
+}
+
+
+
 require_once __DIR__.'/_header.php';
 ?>
 
 <main>
   <form method="POST">
+    <input name="user_email" type="text" placeholder="email">
+    <input name="user_password" type="password" placeholder="password">
     <button>Login</button>
   </form>
 </main>
