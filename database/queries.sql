ba4c122af6262d85bd140982c0640fc3008d2d65
Santiago Donoso <santiagodonoso@gmail.com>
2023-10-12 09:19:33 +0200

    ...

--- database/queries.sql
+++ database/queries.sql
@@ -12,15 +12,55 @@ SELECT * FROM users WHERE user_email = 'admin@company.com'
 SELECT * FROM users
 
 
-
 UPDATE users
 SET user_name = '<script>alert()</script>'
 WHERE user_id = 'd813e27644c19c01cfc4e160268c120e'
 
-
 UPDATE users
 SET user_name = 'Kiera'
 WHERE user_id = '4cc6a5fe22ad1b0f635fee02cab25967'
+
+
+SELECT * FROM users LIMIT 10
+
+
+CREATE VIEW get_10_users AS
+SELECT * FROM users LIMIT 10
+
+
+SELECT * FROM get_10_users
+
+
+DROP VIEW get_10_users
+
+
+-- employees for the salary
+-- users for name and last_name
+
+CREATE VIEW employees_info AS
+SELECT user_name, user_last_name, employee_salary
+FROM users
+JOIN employees 
+ON user_id = employee_id
+
+
+SELECT * FROM employees_info
+
+
+
+SELECT * FROM users WHERE user_id = '71665dc11bf5759f452c26453ed66a47'
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- views/partners.php
+++ views/partners.php
@@ -1,31 +1,70 @@
 <?php
 require_once __DIR__.'/_header.php';
-_is_admin(); // middleware
+if(!_is_admin()){ header('Location: /login'); exit(); };
 
 $db = _db();
-$q = $db->prepare(' SELECT user_id, user_name, user_last_name, user_email 
+$q = $db->prepare(' SELECT user_id, user_name, user_last_name, user_email, user_tag_color
                     FROM users WHERE user_role_name = "partner" LIMIT 10');
 $q->execute();
 $users = $q->fetchAll();                  
 ?>
 
-<main>
-  Partners
+
+<div class="mt-16 pb-20 mr-4 px-4 bg-white rounded-md text-slate-500">
+
+  <div class="flex py-4 text-xl">
+    <h1 class="text-black">
+      Partners
+    </h1>
+    <form action="/search-results" method="GET" class="relative flex items-center ml-auto">
+      <input name="query" type="text" class="pl-7 bg-slate-200">
+      <button class="absolute flex items-center">
+        <span class="material-symbols-outlined ml-1 font-thin">
+          search
+        </span>            
+      </button>
+    </form>
+  </div>
+
+
   <?php if( ! $users ): ?>
-    <h1>No partners in the system</h1>
+    <h1>No employees in the system</h1>
   <?php endif ?>
 
   <?php foreach($users as $user): ?>
-    <div class="">
-      <div class=""><?= $user['user_id'] ?></div>
-      <div class=""><?= $user['user_name'] ?></div>
-      <div class=""><?= $user['user_last_name'] ?></div>
-      <div class=""><?= $user['user_email'] ?></div>
+    <div class="flex items-center gap-4 border-b border-b-slate-200 py-2">
+      <div class="hidden"><?= $user['user_id'] ?></div>
+      <div class="flex items-center justify-center w-10 h-8 text-white text-sm rounded-full" style="background-color: <?php out($user['user_tag_color']); ?>">
+        <?php out($user['user_name'][0]) ?>
+      </div>
+      <div class="w-1/4"><?php out($user['user_name']) ?></div>
+      <div class="w-1/4"><?php out($user['user_last_name']) ?></div>
+      <div class="w-2/5"><?php out($user['user_email']) ?></div>
+      <button class="ml-auto">
+        <span class="material-symbols-outlined mr-2 font-thin">
+          visibility
+        </span>        
+      </button> 
+      <button class="">
+        <span class="material-symbols-outlined mr-2 font-thin">
+            edit_note
+          </span>        
+      </button> 
+      <button class="">
+        <span class="material-symbols-outlined mr-2 font-thin">
+            open_in_new
+          </span>        
+      </button>                
+      <button class="">
+        <span class="material-symbols-outlined mr-2 font-thin">
+            delete
+          </span>        
+      </button>
     </div>
   <?php endforeach ?>
+</div>
 
 
-</main>
 
 <?php
 require_once __DIR__.'/_footer.php';
